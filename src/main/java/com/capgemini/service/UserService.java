86ec6f42a8ee18ffa27060a4e3d3fb3ebd680908
michustarter <michal.ratajczyk@capgemini.com>
2018-07-25 11:54:24 +0200

    *added new feature

--- src/main/java/com/capgemini/service/UserService.java
+++ src/main/java/com/capgemini/service/UserService.java
@@ -3,21 +3,60 @@ package com.capgemini.service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.capgemini.dataaccess.entity.UserEntity;
 import com.capgemini.dataaccess.repository.UserDAO;
+import com.capgemini.exceptions.NullUsersException;
+import com.capgemini.mappers.UserMapper;
 
 @Service
 public class UserService {
-	
+
 	private UserDAO userDAO;
-	
+	private UserMapper userMapper;
+
 	@Autowired
-	public UserService(UserDAO userDAO) {
-		this.userDAO=userDAO;
+	public UserService(UserDAO userDAO, UserMapper userMapper) {
+		this.userDAO = userDAO;
+		this.userMapper = userMapper;
 	}
-	
-	
-	public void addAvailabilityHours(UserDAO userDAO) { //DTO==== np user o nazwie Michal1234 dodaję tutaj
-	//	us
-		
+
+	public UserDTO checkMyProfile(String email) throws NullUsersException {
+		/* konstrukcja wyjątku - ok? */
+		try {
+			UserEntity userEntity = userDAO.findUserByEmail(email);
+			return userMapper.mapToDTOfromDAO(userEntity);
+		} catch (NullUsersException e) {
+			throw new NullUsersException();
+		}
+	}
+
+	public void saveModificationsInUserProfile(UserDTO userDTO) {
+		userDAO.modifyUserEntity(userMapper.mapToDAOfromDTO(userDTO));
+	}
+
+	public UserDTO editMyProfileInformation(UserDTO userDTO, String firstName, String lastName, String email,
+			String oldPassword, String newPassword, String lifeMotto) {
+		if (userDTO == null) {
+			throw new NullUsersException();
+		}
+
+		if (firstName != null) {
+			userDTO.setFirstName(firstName);
+		}
+		if (lastName != null) {
+			userDTO.setLastName(lastName);
+		}
+		if (email != null) {
+			userDTO.setEmail(email);
+		}
+		if (newPassword != null && userDTO.getPassword().equals(oldPassword)) {
+			userDTO.setPassword(newPassword);
+		}
+		if (lifeMotto != null) {
+			userDTO.setLifeMotto(lifeMotto);
+		}
+
+		saveModificationsInUserProfile(userDTO);
+		return userDTO;
 	}
 }
